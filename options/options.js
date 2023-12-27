6fc5533b2ad63fb648eee1a5de62e4f6e10e1ff0
Crowdbotics <team@crowdbotics.com>
2023-12-27 23:40:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-hall-45209",
+  url: "https://wild-hall-45209.botics.co",
+  api: "https://wild-hall-45209.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
